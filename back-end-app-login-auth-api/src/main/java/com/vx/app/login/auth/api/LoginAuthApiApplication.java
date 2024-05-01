51d69a6d989b581d7dba5fe2321777d21454a7f6
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-05-01 20:58:14 -0300

    Estrutura inicial projeto spring-boot

---
+++ back-end-app-login-auth-api/src/main/java/com/vx/app/login/auth/api/LoginAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.vx.app.login.auth.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthApiApplication.class, args);
+	}
+
+}
